20c5d95194a5a174733374376ebb931f414ed0a8
Brandão <zemariabrandao@gmail.com>
2021-05-17 14:13:20 +0100

    Welcome page

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,35 +1,72 @@
+
 async function login() {
 
-    let email = document.getElementById("email").value;
-    let pw = document.getElementById("pw").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
-    if (email != "" && pw != "") { //Verificar se o input do email não está vazio
+    if (username != "" && password != "") {
+
         try {
             
             let utilizador = await $.ajax({
-                url: "/api/utilizadores/login?email="+email+"&password="+pw,
+                url: "/api/users/login?username="+username+"&password="+password,
                 method: "get",
                 dataType: "json"
             });
 
-            if (utilizador.success == 1) {
-                sessionStorage.setItem("user", JSON.stringify(utilizador.data));
-                window.location = "account.html";
-            } 
-            else {
-                alert(utilizador.msg);
-            }
+            sessionStorage.setItem("user", JSON.stringify(utilizador));
+            window.location = "novarota.html";
 
-            } catch (err) {
-                console.log(err);
-                if (err.status == 404) {
-                    alert(err.responseJSON.msg);
-                }
+        } catch (err) {
+            console.log(err);
+            if (err.status == 404) {
+                alert(err.responseJSON.msg);
             }
+        }
 
+    } else {
+        alert("Escreva o seu username e password!");
     }
-    else {
-        alert("Escreve um email/password");
+
+}
+
+
+async function register() {
+
+    let username = document.getElementById("username2").value;
+    let password = document.getElementById("password2").value;
+    let email = document.getElementById("email").value;
+    let gender = document.getElementById("gender").value;
+    let birthday = document.getElementById("birthday").value;
+
+    if (username != "" && password != "" && email != "") {
+
+        try {
+            
+            let info = {
+                username: username,
+                password: password,
+                email: email,
+                gender: gender,
+                birthday: birthday
+            }
+            
+            let utilizador = await $.ajax({
+                url: "/api/users/register",
+                method: "post",
+                data: JSON.stringify(info),
+                contentType: "application/json",
+                dataType: "json"
+            });
+
+            alert("Registado!");
+
+        } catch (err) {
+            console.log(err);
+        }
+
+    } else {
+        alert("Escreva o seu username e password!");
     }
 
 }
--- public/javascripts/rotas.js
+++
@@ -1,7 +0,0 @@
-
-var user = JSON.parse(sessionStorage.getItem("user"));
-window.onload = function() {
-
-    document.getElementById("username").innerHTML = user.user_name;
-
-}
